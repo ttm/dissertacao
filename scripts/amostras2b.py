9283e6af578c6ac882565a9ec9169a9d55b203ea
Green Kobold <greenkobold@gmail.com>
2012-08-21 18:11:47 -0300

    atualizada figura de 2 amostras

--- scripts/amostras2b.py
+++ scripts/amostras2b.py
@@ -4,10 +4,11 @@
 import pylab as p, numpy as n, scikits.audiolab as a
 f=n.fft.fft
 #a=[-1,1]
-a=n.random.rand(2)*2-1 # 2 amostras quaisquer E [-1,1]
-ff=f(a)
+#n2=n.random.rand(2)*2-1 # 2 amostras quaisquer E [-1,1]
+n2=n.array([-0.45599766,  0.77080613])
+ff=f(n2)
 #-1 * cos(n.pi*l)
-p.plot([0,1],a,"bo")
+p.plot([0,1],n2,"bo")
 
 # Primeiro componente, 0Hz
 a0=n.real(ff[0]) 
@@ -18,18 +19,36 @@ ab1=n.abs(ff[1]) # (a**2+b**2)**0.5 = a1 neste caso
 a1=n.real(ff[1]) # eh o modulo ab1 tambem neste caso
 b1=n.imag(ff[1]) # zero para o caso de somente 2 amostras pois a fase eh nula e isen(0)=0
 #fas=n.arctan(b1/a1) # fase fas=n.angle(f[1])
-fas=0 # nao ha adicao de fase para este caso de 2 amostras
-# mas o sinal do a1 => sinal da senóide, inversão 2pi
+fas=0 # não há adição de fase para este caso de 2 amostras
+# mas o sinal do a_1 => sinal da senóide, inversão pi
+# ou seja, se a1<0, n2[0] > n2[1]
 if a1<0: fas+=n.pi # segundo e terceiro quadrantes somam pi
 print("abs: %s, a1: %s, b1: %s, fas: %s" % (ab1,a1,b1,fas))
 
 
-ii=n.linspace(-0.5*n.pi,1.5*n.pi,200)
-iii=n.linspace(-0.5,1.5,200)
-s=(ab1/2)*n.cos(ii+fas)+a0/2
-p.plot(iii,s,"m--")
+ii=n.linspace(-0.5*n.pi,1.5*n.pi,200) # período de oscilação pura
+iii=n.linspace(-0.5,1.5,200) # domínio temporal das amostras 
+s=(ab1/2)*n.cos(ii+fas)+a0/2 # Reconstruindo senóide para o domínio dado
+p.plot(iii,s,"m--") # plotada a senóide passando pelas amostras
 
-#pylab.xticks((-1, 0,  1), ('-1', '0',  '1'), color = 'k', size = 20))
+
+# valores cruciais em y
+p.yticks((0,a0/2-ab1/2,a0/2,a0/2+ab1/2), ('0',r'$\frac{a_0-|a_1|}{2}$',r'$\frac{a_0}{2}$',r'$\frac{a_0+|a_1|}{2}$'), color = 'k', size = 20)
+p.plot((-10,10),(a0/2,)*2,"k--")
+p.plot((-10,10),(a0/2-ab1/2,)*2,"k--")
+p.plot((-10,10),(a0/2+ab1/2,)*2,"k--")
+
+# Para legendas aa direita
+#p.text(2.05, 0.5, r"\bf{level set} $\phi$", {'color' : 'g', 'fontsize' : 20},
+        #horizontalalignment = 'left',
+        #verticalalignment = 'center',
+        #rotation = 90,
+        #clip_on = False)
+#p.text(2.01,a0/2-ab1/2, r'$\frac{a_0-\sqrt{a_1^2+b_1^2}}{2}$', {'color' : 'k', 'fontsize' : 20})
+#p.text(2.01, a0/2, r"$\frac{a_0}{2}$", {'color' : 'k', 'fontsize' : 20})
+#p.text(2.01,a0/2+ab1/2, r'$\frac{a_0+\sqrt{a_1^2+b_1^2}}{2}$', {'color' : 'k', 'fontsize' : 20})
+
+p.text(0,-.8,r"$t_0<t_1 \Leftrightarrow a_1<0$",fontsize=15)
 
 p.xlim(-1.2,2)
 #p.xlim(0,T2*.56)
@@ -38,25 +57,4 @@ p.ylim(-1.1,1.1)
 p.ylabel(r"amplitude $\rightarrow$")
 p.xlabel(r"tempo $\rightarrow$")
 p.show()
-
-
-
-
-d=[-.6,-.2,.2,.6]
-t=[-1,0,1,0]
-q=[-1,-1,1,1]
-
-da1=n.pi
-
-da2=n.pi/2
-
-das=n.linspace(-n.pi/2,-5*n.pi/2,4,endpoint=False)
-s1=n.sin(das)
-s2=n.array(a*2)
-
-#p.plot(s1)
-#p.plot(s2)
-#p.plot(s1+s2)
-#p.plot(d)
-#p.show()
 
