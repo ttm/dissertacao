6627bd07141dcbd336c8a7849b3f04b790f0cfec
Green Kobold <greenkobold@gmail.com>
2012-09-17 03:52:21 -0300

    feita figura para descricao da metrica intra e extra pulso

--- scripts/metricaMusical.py
+++ scripts/metricaMusical.py
@@ -2,13 +2,52 @@
 import numpy as n, pylab as p
 
 resolucao=9*8 # 72
+#axL = p.subplot(2,1,1)
+fig=p.figure()
+ax1=fig.add_subplot(111)
+ax2=ax1.twinx()
 
-p.plot((0,34),(1,1)); p.plot((36,72),(1,1))
+#p.plot((0,34),(1,1)); p.plot((36,72),(1,1))
 
-p.plot((0,16),(2,2)); p.plot((36,52),(2,2))
-p.plot((18,34),(2,2)); p.plot((54,72),(2,2))
+#p.plot((0,16),(2,2)); p.plot((36,52),(2,2))
+#p.plot((18,34),(2,2)); p.plot((54,72),(2,2))
 
-#p.plot(xs2,s2)
-p.ylim(-1,4)
+ordem=[0,1,4,2,-1,5,-2,3,6]
+colors=['g','b','r','b','k','r','k','b','r']
+
+for i in xrange(9):
+    for j in xrange(i+1):
+        p.plot((j*resolucao/(i+1), (j+1)*resolucao/(i+1)-2),(ordem[i],ordem[i]),c=colors[i%len(colors)], linewidth=3)
+
+p.plot((-1000,-1000),'r',linewidth=3,label=u'compassos compostos, divisões ternárias, modus perfectus')
+p.plot((-1000,-1000),'b',linewidth=3,label=u'compassos simples, divisões binárias, modus imperfectus')
+p.plot((-1000,-1000),'k',linewidth=3,label=u'compassos em 5 e 7, quiálteras de 5 e 7')
+p.legend(loc="upper right")
+
+
+ax1.set_ylim(-3,8)
+ax2.set_ylim(-3,8)
+p.xlim(-4,74)
+
+ax1.set_xticks(())
+ax1.set_ylabel(r"$\leftarrow$"+u"divisões do pulso musical"+r"$\rightarrow$")
+ax2.set_ylabel(r"$\leftarrow$"+u"fórmulas de compasso"+r"$\rightarrow$")
+
+ax1.set_yticks((-2,-1,0,1,2,3,4,5,6))
+ax1.set_yticklabels((r"$heptina\;de\;semicolcheia$",r"$quintina\;de\;semicolcheia$",r"$seminima$",r"$duas\;colcheias$",r"$quatro\;semicolcheias$",r"$oito\;fusas$", r"$tercina\;de\;colcheias$",r"$sextina\;de\;semicolcheias$",r"$nonina\;de\;semicolcheias$"))
+
+ax2.set_yticks((-2,-1,0,1,2,3,4,5,6))
+ax2.set_yticklabels((r"$.^7_4$",r"$.^5_4$",r"$(.^1_4)$",r"$.^2_4$",r"$.^4_4$",r"$.^8_4$",r"$.^3_4$",r"$.^6_8$",r"$.^9_8$"))
+#axR=p.subplot(2,1,2,sharex=axL, frameon=False)
+#axR.yaxis.set_label_position("right")
+
+#ax.yaxis.set_ticks_position("right")
+#axR.yaxis.tick_right()
+
+
+#p.yticks((-1,0,1,2,3,4,5,6),(r"$-\delta_a$",r"$0$",r"$\delta_a$",r"$2\delta_a$",r"$3\delta_a$",r"$4\delta_a$",r"$5\delta_a$",r"$6\delta_a$"))
+
+#p.ylabel(r"amplitude $\rightarrow$")
+ax1.set_xlabel(r"tempo $\rightarrow$")
 p.show()
 
