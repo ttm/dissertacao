b6e9cfa180c9c437beed7a4ef5c1d307687a6d43
Green Kobold <renato.fabbri@gmail.com>
2012-11-04 17:16:25 -0200

    alteracoes minimas nos comentarios do script de vbr

--- scripts/vibrato.py
+++ scripts/vibrato.py
@@ -1,13 +1,13 @@
 #-*- coding: utf8 -*-
 import numpy as n, pylab as p, scikits.audiolab as a
 
-fa=44100
+fa=44100 # frequência de amostragem
 Dv=2048 # tamanho da tabela do vibrato
 fv=1.5 # frequência do vibrato
 nu=12. # desvio maximo em semitons do vibrato (profundidade)
 f=1040. # freq do som em si
 D=1024 # tamanho da tabela do som
-dur=2
+dur=2 # duração em segundos
 
 x=n.linspace(0,2*n.pi,Dv,endpoint=False)
 tabv=n.sin(x) # tabela senoidal para o vibrato
@@ -22,9 +22,9 @@ fi=f*(  2.**(  tv/12.  )   ) # frequência em Hz em cada amostra
 tab=n.linspace(-1,1,D) # dente de serra
 
 dD=fi*(D/float(fa)) # a movimentação na tabela por amostra
-gi=n.cumsum(dD,0,n.int) # a movimentacao na tabela total, jah inteiro
-ti=tabv[gi%D]
-a.wavwrite(ti,"vibrato.wav",fa)
+gi=n.cumsum(dD,0,n.int) # a movimentação na tabela total, já os índices
+ti=tabv[gi%D] # busca dos índices na tabela
+a.wavwrite(ti,"vibrato.wav",fa) # escrita do som
 
 gi=n.array(  ii * (D/float(fa)) * f  , n.int ) % Dv
 t=tabv[ gi ]
