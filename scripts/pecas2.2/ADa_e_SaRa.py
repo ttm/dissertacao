c08d2ae6b2a9a3451f5ec8ade72a0acdad4acc59
Green Kobold <renato.fabbri@gmail.com>
2012-11-16 05:25:54 -0200

    pecas quase prontas

--- scripts/pecas2.2/ADa_e_SaRa.py
+++ scripts/pecas2.2/ADa_e_SaRa.py
@@ -51,23 +51,24 @@ def A(fa=2.,V_dB=10.,d=2.,taba=S_i):
     A_i=A_i*10.**(V_dB/20.)
     return A_i
 import pylab as p
+
 def adsr(som,A=10.,D=20.,S=-20.,R=100.,xi=1e-2):
     a_S=10**(S/20.)
     Lambda=len(som)
-    Lambda_A=int(A*f_a*0.01)
-    Lambda_D=int(D*f_a*0.01)
-    Lambda_R=int(R*f_a*0.01)
+    Lambda_A=int(A*f_a*0.001)
+    Lambda_D=int(D*f_a*0.001)
+    Lambda_R=int(R*f_a*0.001)
 
     ii=n.arange(Lambda_A,dtype=n.float)
-    A=xi*(1./xi)**(ii/(Lambda_A-1))-1
+    A=ii/(Lambda_A-1)
     A_i=A
     ii=n.arange(Lambda_A,Lambda_D+Lambda_A,dtype=n.float)
-    D=a_S**(   ( ii-Lambda_A )/( Lambda_D-1) )
+    D=1-(1-a_S)*(   ( ii-Lambda_A )/( Lambda_D-1) )
     A_i=n.hstack(  (A_i, D  )   )
     S=n.ones(Lambda-Lambda_R-(Lambda_A+Lambda_D),dtype=n.float)*a_S
     A_i=n.hstack( ( A_i, S )  )
     ii=n.arange(Lambda-Lambda_R,Lambda,dtype=n.float)
-    R=a_S*(xi/a_S)**((ii-(Lambda-Lambda_R))/(Lambda_R-1))
+    R=a_S-a_S*((ii-(Lambda-Lambda_R))/(Lambda_R-1))
     A_i=n.hstack(  (A_i,R)  )
     p.plot(A_i)
 
@@ -80,20 +81,139 @@ def adsr(som,A=10.,D=20.,S=-20.,R=100.,xi=1e-2):
 
 #T=v(tabv=Tr_i ,d=2.,fv=35.,nu=7.0)*A()
 #T2=v(tabv=Tr_i ,d=2.,fv=0.,nu=7.0)*A()
+T=adsr(v(tabv=Tr_i ,d=2.,fv=1.,nu=0.),10,10,-10.)
 
-T=adsr(v(tabv=Tr_i ,d=2.,fv=35.,nu=7.0)*A(),100,100,xi=1e-1)
-T2=adsr(v(tabv=Tr_i ,d=2.,fv=0.,nu=7.0)*A(),100,100,xi=1e-1)
 
-T_i=n.hstack((T,T2))
-
-T=v(tabv=Tr_i ,d=2.,fv=35.,nu=7.0)*A()
-T2=v(tabv=Tr_i ,d=2.,fv=0.,nu=7.0)*A()
-
-T_i=n.hstack((T_i,T,T2))
-
+T_i=n.hstack((n.zeros(f_a),adsr(v(tabv=Tr_i ,d=2.,fv=10.,nu=7.0),10,10,-5.),T))
 
 T_i=(T_i-T_i.min())/(T_i.max()-T_i.min())
 
 a.wavwrite(T_i,"ADa_e_SaRa.wav",f_a) # escrita do som
 
+
+############3
+#T=v(tabv=Tr_i ,d=2.,fv=35.,nu=7.0)*A()
+#T2=v(tabv=Tr_i ,d=2.,fv=0.,nu=7.0)*A()
+T=adsr(v(tabv=Tr_i ,d=2.,fv=1.,nu=0.),10,10,-10.)
+T1=adsr(v(tabv=Tr_i ,d=2.,fv=1.,nu=0.),5,10,-10.)
+T2=adsr(v(tabv=Tr_i ,d=2.,fv=1.,nu=0.),5,5,-10.)
+T3=adsr(v(tabv=Tr_i ,d=2.,fv=1.,nu=0.),2,10,-10.)
+
+T_i=n.hstack((n.zeros(f_a),T,T1,T2,T3))
+
+T=adsr(v(tabv=Tr_i ,d=2.,fv=3.,  nu=3.),10,10,-10.)
+T1=adsr(v(tabv=Tr_i ,d=2.,fv=1., nu=3.),5,10,-10.)
+T2=adsr(v(tabv=Tr_i ,d=2.,fv=6., nu=3.),5,5,-10.)
+T3=adsr(v(tabv=Tr_i ,d=2.,fv=19.,nu=3.),2,10,-10.)
+
+T_i=n.hstack((T_i,T,T1,T2,T3))
+
+
+
+T_i=(T_i-T_i.min())/(T_i.max()-T_i.min())
+
+a.wavwrite(T_i,"ADa_e_SaRa2.wav",f_a) # escrita do som
+
+
+############3
+#T=v(tabv=Tr_i ,d=2.,fv=35.,nu=7.0)*A()
+#T2=v(tabv=Tr_i ,d=2.,fv=0.,nu=7.0)*A()
+T= adsr(v(tab=Tr_i,tabv=Tr_i ,d=2.,fv=1.,nu=0.),10,10,-10.)
+T1=adsr(v(tab=Tr_i,tabv=Tr_i ,d=2.,fv=1.,nu=0.),5,10,-10.)
+T2=adsr(v(tab=Tr_i,tabv=Tr_i ,d=2.,fv=1.,nu=0.),5,5,-10.)
+T3=adsr(v(tab=Tr_i,tabv=Tr_i ,d=2.,fv=1.,nu=0.),2,10,-10.)
+
+T_i=n.hstack((n.zeros(f_a),T,T1,T2,T3))
+
+T=adsr( v(tab=Tr_i,tabv=Tr_i ,d=2.,fv=3.,  nu=3.),10,10,-10.)
+T1=adsr(v(tab=Tr_i,tabv=Tr_i ,d=2.,fv=1., nu=3.),5,10,-10.)
+T2=adsr(v(tab=Tr_i,tabv=Tr_i ,d=2.,fv=6., nu=3.),5,5,-10.)
+T3=adsr(v(tab=Tr_i,tabv=Tr_i ,d=2.,fv=19.,nu=3.),2,10,-10.)
+
+T_i=n.hstack((T_i,T,T1,T2,T3))
+
+
+
+T_i=(T_i-T_i.min())/(T_i.max()-T_i.min())
+
+a.wavwrite(T_i,"ADa_e_SaRa3.wav",f_a) # escrita do som
+
+
+############3
+#T=v(tabv=Tr_i ,d=2.,fv=35.,nu=7.0)*A()
+#T2=v(tabv=Tr_i ,d=2.,fv=0.,nu=7.0)*A()
+T= adsr(v(tab=Tr_i,tabv=Tr_i ,d=2.,fv=1.,nu=0.),2.,10,-10.)
+T1=adsr(v(tab=Tr_i,tabv=Tr_i ,d=2.,fv=1.,nu=0.),1.,10.,-10.)
+T2=adsr(v(tab=Tr_i,tabv=Tr_i ,d=2.,fv=1.,nu=0.),0.5, 5.,-10.)
+T3=adsr(v(tab=Tr_i,tabv=Tr_i ,d=2.,fv=1.,nu=0.),0.2,10.,-10.)
+
+T_i=n.hstack((n.zeros(f_a),T,T1,T2,T3))
+
+T=adsr( v(tab=Tr_i,tabv=Tr_i ,d=2.,fv=3.,  nu=3.),2,  5.,-10.)
+T1=adsr(v(tab=Tr_i,tabv=Tr_i ,d=2.,fv=1., nu=3.) ,1,  2,-10.)
+T2=adsr(v(tab=Tr_i,tabv=Tr_i ,d=2.,fv=6., nu=3.) ,0.5,1.,-10.)
+T3=adsr(v(tab=Tr_i,tabv=Tr_i ,d=2.,fv=19.,nu=3.) ,0.2,0.5,-10.)
+
+T_i=n.hstack((T_i,T,T1,T2,T3))
+
+
+
+T_i=(T_i-T_i.min())/(T_i.max()-T_i.min())
+
+a.wavwrite(T_i,"ADa_e_SaRa4.wav",f_a) # escrita do som
+
+##########3
+T=adsr( v(tab=Tr_i,tabv=Tr_i ,d=2.,fv=3.,  nu=3.),2,  5.,-10,1000.)
+T1=adsr(v(tab=Tr_i,tabv=Tr_i ,d=2.,fv=1., nu=3.) ,1,  2,-5,500.)
+T2=adsr(v(tab=Tr_i,tabv=Tr_i ,d=2.,fv=6., nu=3.) ,0.5,1.,-20,200.)
+T3=adsr(v(tab=Tr_i,tabv=Tr_i ,d=2.,fv=19.,nu=3.) ,0.2,0.5,-30,1500.)
+
+T_i=n.hstack((T,T1,T2,T3))
+
+T=adsr( v(tab=Tr_i,tabv=Tr_i ,d=2.,fv=3.,  nu=3.),2,  50.,-10,1000.)
+T1=adsr(v(tab=Tr_i,tabv=Tr_i ,d=2.,fv=1., nu=3.) ,1,  200,-2,500.)
+T2=adsr(v(tab=Tr_i,tabv=Tr_i ,d=2.,fv=6., nu=3.) ,0.5,1000.,-15,200.)
+T3=adsr(v(tab=Tr_i,tabv=Tr_i ,d=2.,fv=19.,nu=3.) ,0.2,300.,-25,1500.)
+
+T_i=n.hstack((T_i,T,T1,T2,T3))
+
+
+
+
+T_i=(T_i-T_i.min())/(T_i.max()-T_i.min())
+
+a.wavwrite(T_i,"ADa_e_SaRa5.wav",f_a) # escrita do som
+
+
+###########3
+
+T1=adsr(v(tab=Tr_i,tabv=Tr_i ,d=2.,fv=6., nu=3.) ,1.,1.,-20,200.)
+T2=adsr(v(tab=Tr_i,tabv=Tr_i ,d=2.,fv=19.,nu=3.) ,5.,5.,-30,1500.)
+T3=adsr(v(tab=Tr_i,tabv=Tr_i ,d=2.,fv=16., nu=6.) ,1.,2.,-10,200.)
+T4=adsr(v(tab=Tr_i,tabv=Tr_i ,d=2.,fv=19.,nu=3.) ,7.,7.,-30,1500.)
+
+T_i=n.hstack((n.zeros(f_a),T1,T2,T3,T4))
+T_i=(T_i-T_i.min())/(T_i.max()-T_i.min())
+
+a.wavwrite(T_i,"ADa_e_SaRa6.wav",f_a) # escrita do som
+
+##################
+T1=adsr(v(tab=Tr_i,tabv=Tr_i ,d=2.,fv=6., nu=3.) ,1.,1.,-20,200.)
+T2=adsr(v(tab=Tr_i,tabv=Tr_i ,d=2.,fv=19.,nu=3.) ,5.,5.,-30,1500.)
+T3=adsr(v(tab=Tr_i,tabv=Tr_i ,d=2.,fv=16., nu=6.) ,1.,2.,-10,200.)
+T4=adsr(v(tab=Tr_i,tabv=Tr_i ,d=2.,fv=19.,nu=3.) ,7.,7.,-30,1500.)
+
+T_i=n.hstack((n.zeros(f_a),T1,T2,T3,T4))
+
+
+T1=adsr(v(tabv=Tr_i ,d=2.,fv=6., nu=3.) ,1.,1.,-20,200.)
+T2=adsr(v(tabv=Tr_i ,d=2.,fv=19.,nu=3.) ,5.,5.,-30,1500.)
+T3=adsr(v(tabv=Tr_i ,d=2.,fv=16., nu=6.) ,1.,2.,-10,200.)
+T4=adsr(v(tabv=Tr_i ,d=2.,fv=19.,nu=3.) ,7.,7.,-30,1500.)
+
+
+T_i=n.hstack((T_i,T1,T2,T3,T4))
+T_i=(T_i-T_i.min())/(T_i.max()-T_i.min())
+
+a.wavwrite(T_i,"ADa_e_SaRa7.wav",f_a) # escrita do som
 
---
+++ scripts/pecas2.2/chorusInfantil.py
@@ -0,0 +1,160 @@
+#-*- coding: utf8 -*-
+import numpy as n, scikits.audiolab as a
+H=n.hstack
+V=n.vstack
+
+f_a = 44100. # Hz, frequência de amostragem
+
+############## 2.2.1 Tabela de busca (LUT)
+Lambda_tilde=Lt=1024.
+
+# Senoide
+foo=n.linspace(0,2*n.pi,Lt,endpoint=False)
+S_i=n.sin(foo) # um período da senóide com T amostras
+
+# Quadrada:
+Q_i=n.hstack(  ( n.ones(Lt/2)*-1 , n.ones(Lt/2) )  )
+
+# Triangular:
+foo=n.linspace(-1,1,Lt/2,endpoint=False)
+Tr_i=n.hstack(  ( foo , foo*-1 )   )
+
+# Dente de Serra:
+D_i=n.linspace(-1,1,Lt)
+
+
+def v(f=200,d=2.,tab=S_i,fv=2.,nu=2.,tabv=S_i):
+    Lambda=n.floor(f_a*d)
+    ii=n.arange(Lambda)
+    Lv=float(len(S_i))
+
+    Gammav_i=n.floor(ii*fv*Lv/f_a) # índices para a LUT
+    Gammav_i=n.array(Gammav_i,n.int)
+    Tv_i=tabv[Gammav_i%int(Lv)] # padrão de variação do vibrato para cada amostra
+
+    F_i=f*(   2.**(  Tv_i*nu/12.  )   ) # frequência em Hz em cada amostra
+
+    D_gamma_i=F_i*(Lt/float(f_a)) # a movimentação na tabela por amostra
+    Gamma_i=n.cumsum(D_gamma_i) # a movimentação na tabela total
+    Gamma_i=n.floor( Gamma_i) # já os índices
+    Gamma_i=n.array( Gamma_i, dtype=n.int) # já os índices
+    return tab[Gamma_i%int(Lt)] # busca dos índices na tabela
+
+def A(fa=2.,V_dB=10.,d=2.,taba=S_i):
+    Lambda=n.floor(f_a*d)
+    ii=n.arange(Lambda)
+    Lt=float(len(taba))
+    Gammaa_i=n.floor(ii*fa*Lt/f_a) # índices para a LUT
+    Gammaa_i=n.array(Gammaa_i,n.int)
+### 2.55 padrão de oscilação do vibrato
+    A_i=taba[Gammaa_i%int(Lt)] # padrão de variação da amplitude do tremolo para cada amostra
+    A_i=A_i*10.**(V_dB/20.)
+    return A_i
+
+
+def adsr(som,A=10.,D=20.,S=-20.,R=100.,xi=1e-2):
+    a_S=10**(S/20.)
+    Lambda=len(som)
+    Lambda_A=int(A*f_a*0.001)
+    Lambda_D=int(D*f_a*0.001)
+    Lambda_R=int(R*f_a*0.001)
+
+    ii=n.arange(Lambda_A,dtype=n.float)
+    A=ii/(Lambda_A-1)
+    A_i=A
+    ii=n.arange(Lambda_A,Lambda_D+Lambda_A,dtype=n.float)
+    D=1-(1-a_S)*(   ( ii-Lambda_A )/( Lambda_D-1) )
+    A_i=n.hstack(  (A_i, D  )   )
+    S=n.ones(Lambda-Lambda_R-(Lambda_A+Lambda_D),dtype=n.float)*a_S
+    A_i=n.hstack( ( A_i, S )  )
+    ii=n.arange(Lambda-Lambda_R,Lambda,dtype=n.float)
+    R=a_S-a_S*((ii-(Lambda-Lambda_R))/(Lambda_R-1))
+    A_i=n.hstack(  (A_i,R)  )
+
+    return som*A_i
+
+
+s=v(f=200.,nu=0.)
+s0=n.copy(s)
+amb=2**(.1/12.)
+f=200.
+amb=2**(.00/12.)*f
+amb_trem=.5
+for i in xrange(10):
+    f_=f+amb*n.random.random()-amb/2.
+    s+=v(f=_,nu=n.random.random()*amb_trem-amb_trem/2)
+
+T_i=n.hstack((s0,s))
+
+T_i=((T_i-T_i.min())/(T_i.max()-T_i.min()))*2-1
+        
+a.wavwrite(T_i,"chorusInfantil.wav",f_a) # escrita do som
+
+
+s=v(f=200.,nu=0.)
+s0=n.copy(s)
+
+amb=2**(.1/12.)
+f=200.
+amb=2**(.00/12.)*f
+amb_trem=.5
+for i in xrange(10):
+    f_=f+amb*n.random.random()-amb/2.
+    s+=v(   f=f_,   nu=n.random.random()*amb_trem-amb_trem/2.    )
+
+T_i=n.hstack((s0,s))
+
+
+s=v(f=200.,nu=0.)
+s0=n.copy(s)
+amb=2**(.01/12.)
+f=200.
+amb=2**(.00/12.)*f
+amb_trem=.5
+for i in xrange(10):
+    f_=f+amb*n.random.random()-amb/2.
+    s+=v(  f_,   nu=n.random.random()*amb_trem-amb_trem/2.    )
+
+T_i=n.hstack((T_i,s0,s))
+
+s=v(f=200.,nu=0.)
+s0=n.copy(s)
+amb=2**(.01/12.)
+f=200.
+amb=2**(.00/12.)*f
+amb_trem=.1
+for i in xrange(10):
+    s+=v(   f=200.*(n.random.random()*amb-amb/2.),   nu=n.random.random()*amb_trem-amb_trem/2.    )
+
+T_i=n.hstack((T_i,s0,s))
+
+s=v(f=200.,nu=0.)
+s0=n.copy(s)
+amb=2**(.01/12.)
+f=200.
+amb=2**(.00/12.)*f
+amb_trem=.0
+for i in xrange(10):
+    s+=v(   f=200.*(n.random.random()*amb-amb/2.),   nu=n.random.random()*amb_trem-amb_trem/2.    )
+
+T_i=n.hstack((T_i,s0,s))
+
+s=v(f=200.,nu=0.)
+s0=n.copy(s)
+f=200.
+amb=2**(.00/12.)*f
+f=200.
+amb=2**(.00/12.)*f
+amb_trem=.0
+for i in xrange(10):
+    f_=f+amb*n.random.random()-amb/2.
+    s+=v(   f=f_,   nu=n.random.random()*amb_trem-amb_trem/2.    )
+
+T_i=n.hstack((T_i,s0,s))
+
+
+T_i=((T_i-T_i.min())/(T_i.max()-T_i.min()))*2-1
+        
+a.wavwrite(T_i,"chorusInfantil3.wav",f_a) # escrita do som
+
+
