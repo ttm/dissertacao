f25545f0e1cc65746b1c396145a782755d4c7cc1
Green Kobold <renato.fabbri@gmail.com>
2013-04-10 22:02:26 -0300

    adicionado script que plota 2 figuras com o espectro de notas na tonica, dominante e subdominante

---
+++ scripts/harmoniaEspectral.py
@@ -0,0 +1,18 @@
+import numpy as n, pylab as p
+
+tonica=n.array([0,4,7])
+dominante=n.array([2,5,7])
+subdominante=n.array([0,5,9])
+aa=n.random.randint(0,3,2**16)
+tonica[aa]
+dominante[aa]
+subdominante[aa]
+trecho=n.hstack((tonica[aa],dominante[aa],subdominante[aa],dominante[aa],tonica[aa]))
+p.specgram(trecho)
+p.savefig("espectro_harmonia.png")
+
+t_=440.*(2.**(trecho/12.))
+
+p.specgram(t_)
+p.savefig("espectro_harmonia2.png")
+p.show()
