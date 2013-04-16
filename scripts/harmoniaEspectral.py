52552c9c3b4d7fe82734b6da84c593d68d2b5afb
Green Kobold <renato.fabbri@gmail.com>
2013-04-16 03:03:34 -0300

    feita implementação de efeito Doppler e descrição na secao 2.2

--- scripts/harmoniaEspectral.py
+++ scripts/harmoniaEspectral.py
@@ -1,7 +1,7 @@
 import numpy as n, pylab as p
 
 tonica=n.array([0,4,7])
-dominante=n.array([2,5,7])
+dominante=n.array([2,7,11])
 subdominante=n.array([0,5,9])
 aa=n.random.randint(0,3,2**16)
 tonica[aa]
